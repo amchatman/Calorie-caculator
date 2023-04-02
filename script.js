08310c832be63a255bc6ad64167f63e82e8d9714
Ashley Chatman <ashchat2008@gmail.com>
2023-04-02 15:32:32 -0400

    Comments -Add comments

---
+++ script.js
@@ -0,0 +1,5 @@
+/* 1.Get input of Protein, Carbs and Fats
+   2.Add total of Protein, Carbs and Fats in each section
+   3.Get percentage of total for each input base on how items store
+   4. Input: Example Description: Name , Protein: 0g, Carbs: 0, Fats: 0
+   */
