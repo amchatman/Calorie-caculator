673b87bb25dd22f5d782f56c2bd8a4afb517809a
Ashley Chatman <ashchat2008@gmail.com>
2023-04-05 12:39:42 -0400

    Input -Remove span -Add min to input -Add array

--- src/script.js
+++ src/script.js
@@ -4,3 +4,11 @@
    4. Input: Example Description: Name , Protein: 0g, Carbs: 0, Fats: 0
    5. Get Current date Month, date, year
    */
+
+//Get current Month, Date, and Year
+const month = currentDate.getMonth();
+const date = currentDate.getDate();
+const year = currentDate.getYear();
+
+const nameMonth = ['January','Feburary','March','April','May','June'
+,'July','August','September','November','December']
