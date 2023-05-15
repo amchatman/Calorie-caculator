5487cb13e7d6bdac0e5b30ca6feb507b48ba2792
Ashley Chatman <ashchat2008@gmail.com>
2023-05-15 19:15:15 -0400

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+  './pages/**/*.{html,js}',
+  './components/**/*.{html,js}'
+],
   theme: {
     fontFamily:{
       'display':["Roboto Mono", "monospace"],
